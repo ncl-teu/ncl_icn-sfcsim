f3e37ee1a74c64471d6d8c4e93c25fc0c72237cd
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-11-11 14:07:58 +0900

    Add files via upload

--- src/net/gripps/ccn/icnsfc/main/AutoMain.java
+++ src/net/gripps/ccn/icnsfc/main/AutoMain.java
@@ -1,5 +1,6 @@
 package net.gripps.ccn.icnsfc.main;
 
+import net.gripps.ccn.Logger.CCNLog;
 import net.gripps.ccn.core.CCNRouter;
 import net.gripps.ccn.icnsfc.AutoUtil;
 import net.gripps.ccn.icnsfc.core.AutoEnvironment;
@@ -25,10 +26,10 @@ public class AutoMain {
 */
             //欲しい情報のカテゴリ：startVNF開始,
 
-            ISLog.getIns().log(",Int., 0:Interest/1:CacheHit, SFCID, prefix, fromSFID@R_ID, toSFID, <-R/N+ID, Hop, Duration, @Host, @VM, @vCPU, TimeStamp");
+            ISLog.getIns().log(",Int., 0:Interest/1:CacheHit, AplID, SFCID, prefix, fromSFID@R_ID, toSFID, <-R/N+ID, Hop, Duration, @Host, @VM, @vCPU, TimeStamp");
             ISLog.getIns().log(",Data., 0: ProcReturn/1: CacheReturn, SFCID, prefix, Proc.Time, fromSFID, R_ID->, toSFID@R/N+ID, Hop, " +
                     "ComTime, DataSize, @Host, @VM, @vCPU, BW, #ofSharedConnection, TimeStamp");
-
+            CCNLog.setIsSFCMode(true);
             //設定ファイルを取得
             String fileName = args[0];
             //Utilの初期化（設定ファイルの値の読み込み）
