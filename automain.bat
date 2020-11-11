ae1d3a4f0d820d6bc8a47c688b905a77d62604ae
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-11-11 14:10:56 +0900

    Add files via upload

---
+++ automain.bat
@@ -0,0 +1,2 @@
+java -Xmn500m -Xmx1000m -Xms1000m -cp  ./classes;./is;lib/ncl-taskschedsim.jar;lib/ncl-sfcworkflow.jar;lib/commons-net-3.6.jar;lib/commons-math-2.0.jar;lib/log4j-api-2.11.1.jar;lib/log4j-core-2.11.1.jar net.gripps.ccn.icnsfc.main.AutoMain  auto.properties
+pause;
